ac34fde14e177219ca920ee1c9102df7d89486e0
yuya-yuya <yytyfp5115@icloud.com>
2021-01-01 15:53:43 +0900

    initial commit

--- main.js
+++ main.js
@@ -1,6 +1,50 @@
 var app = new Vue({
-  el: "#app",
+  el: '#app',
   data: {
-      message: 'Hello Vue.js'
+      list: [],
+      addText: '',
+  },
+  //watchでlistの変更を監視
+  watch: {
+      list: {
+          handler: function() {
+              //localStorageにデータを保存
+              localStorage.setItem("list", JSON.stringify(this.list));
+          },
+          deep: true
+      }
+  },
+  //マウントされた時にlocalStorageからデータを取得
+  mounted: function() {
+      this.list = JSON.parse(localStorage.getItem("list")) || [];
+  },
+  methods: {
+      addToDo: function() {
+          if (this.addText !== '') {
+              this.list.push({
+                  text: this.addText, 
+                  isChecked: false,
+              });
+          }
+          this.addText = '';
+      },
+      deleteBtn: function() {
+          this.list = this.list.filter(function(todo) {
+              return !todo.isChecked;
+          });
+      }
+  },
+  computed:{
+    remaining: function(){
+        var count = 0;
+        var list = this.list;
+        var length = list.length;
+        for(var i = 0 ; i < length ; i++){
+            if(!list[i].isChecked){
+                count++;
+            }
+        }
+        return count;
+    }
   }
 });
